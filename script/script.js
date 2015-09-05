ed43d9c3b21256fc8f75e657c9f76bd8185318c4
d1maall <d1maall1606@gmail.com>
2015-09-05 22:18:45 +0300

    lots of new stuff in this commit

--- script/script.js
+++ script/script.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+
+    var myMap;
+    ymaps.ready(init);
+    function init () {
+        // Создание экземпляра карты и его привязка к контейнеру с
+        // заданным id ("map").
+        myMap = new ymaps.Map('map', {
+            // При инициализации карты обязательно нужно указать
+            // её центр и коэффициент масштабирования.
+            center: [55.76, 37.64], // Москва
+            zoom: 12,
+            controls: []
+        });
+        myMap.behaviors.disable('scrollZoom');
+        var zoomControl = new ymaps.control.ZoomControl({
+            options: {
+                size: "small"
+            }
+        });
+        myMap.controls.add(zoomControl);
+
+    }
+
     $(window).scroll(function (event) {
         if ($(window).scrollTop() > 0) {
             $(".start-screen__bouncing-arrow").fadeOut();
