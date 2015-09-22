540ed3d93a7f7dca3a45b0b660e5436371c7227d
d1maall <d1maall1606@gmail.com>
2015-09-22 20:39:33 +0300

    — playing around with Gulp build

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    uncss = require('gulp-uncss'),
+    minifyCSS = require('gulp-minify-css'),
+    jsmin = require('gulp-jsmin'),
+    rename = require('gulp-rename'),
+    htmlmin = require('gulp-htmlmin');
+    /*notify = require("gulp-notify");*/
+
+gulp.task('default', function() {
+    gulp.src('./style/*.css')
+        .pipe(uncss({
+            html: ['*.html']
+        }))
+        //.pipe(rename({suffix: '.min'}))
+        //.pipe(minifyCSS())
+        .pipe(gulp.dest('./out/style'));
+
+    gulp.src('./script/*.js')
+        .pipe(jsmin())
+        //.pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./out/script'));
+
+    gulp.src('*.html')
+        .pipe(htmlmin())
+        .pipe(gulp.dest('./out/'));
+});
